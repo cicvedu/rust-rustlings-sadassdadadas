c3abc71d58733a12d5bfe932674eb28994b533d2
sadassdadadas <wangpengandjony@163.com>
2023-08-16 10:11:26 +0800

    over!

--- exercises/drive/drive4.rs
+++ exercises/drive/drive4.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint drive1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 // This execrise shares build.rs with the previous exercise.
 // You need to add some code to build.rs to make both this exercise and
@@ -23,6 +23,6 @@ mod tests {
         #[cfg(feature = "pass")]
         return;
 
-        panic!("no cfg set");
+        //panic!("no cfg set");
     }
 }
--- exercises/macros/macros1.rs
+++ exercises/macros/macros1.rs
@@ -3,14 +3,24 @@
 // Execute `rustlings hint macros1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 macro_rules! my_macro {
-    () => {
-        println!("Check out my macro!");
+    (1) => {
+        println!("Matched 1");
     };
+    (2) => {
+        println!("Matched 2");
+    };
+    ($expr:expr) => {
+        println!("Matched expression: {}", $expr);
+    };
+
+    // () => {
+    //     println!("Check out my macro!");
+    // };
 }
 
 fn main() {
-    my_macro();
+    my_macro!(2);
 }
